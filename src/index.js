d214d5a0fd18c0dda3eca239b69b3b23d8b5bdc5
christian-dale <jchaukvik@gmail.com>
2026-01-26 19:38:08 +0100

    Move svg code to its own method and refactor

--- src/index.js
+++ src/index.js
@@ -2,26 +2,9 @@ import { escape } from "html-escaper";
 
 export default {
     async fetch(request, env, ctx) {
-        let motd = escape(
-            await env.KV.get("MOTD") ?? await this.updateMOTD(env)
-        );
+        const motd = await env.KV.get("MOTD") ?? await this.updateMOTD(env);
 
-        const svg = `
-            <svg width="500" height="70" xmlns="http://www.w3.org/2000/svg">
-                    <foreignObject x="0" y="0" width="100%" height="50">
-                        <div xmlns="http://www.w3.org/1999/xhtml"
-                            style="font-family: monospace; font-size: 12px; color: #02c39a; text-align: left;">
-                            ${motd}
-                        </div>
-                    </foreignObject>
-
-                    <text x="0" y="50" font-family="monospace" font-size="9" fill="#078b6e">
-                        🤖 AI-generated daily quote
-                    </text>
-                </svg>
-        `;
-
-        return new Response(svg, {
+        return new Response(this.buildSVG(motd), {
             headers: {
                 "Content-Type": "image/svg+xml",
                 "Cache-Control": "no-cache, no-store, must-revalidate"
@@ -51,6 +34,23 @@ export default {
 
         await env.KV.put("MOTD", res.response);
 
-        return await env.KV.get("MOTD");
+        return res.response;
+    },
+
+    buildSVG(text) {
+        return `
+            <svg width="500" height="70" xmlns="http://www.w3.org/2000/svg">
+                <foreignObject x="0" y="0" width="100%" height="50">
+                    <div xmlns="http://www.w3.org/1999/xhtml"
+                        style="font-family: monospace; font-size: 12px; color: #02c39a; text-align: left;">
+                        ${escape(text)}
+                    </div>
+                </foreignObject>
+
+                <text x="0" y="50" font-family="monospace" font-size="9" fill="#078b6e">
+                    🤖 Auto-generated daily quote
+                </text>
+            </svg>
+        `;
     }
 };
