857f6a0e69d1a73392566d2c225c9c05cec096d0
christian-dale <jchaukvik@gmail.com>
2026-02-11 22:56:59 +0100

    refactor: migrate to typescript and add type definitions

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
 import { escape } from "html-escaper";
+import { ExportedHandler, KVNamespace, Ai, ExecutionContext, ScheduledController } from "@cloudflare/workers-types";
+
+interface Env {
+    KV: KVNamespace;
+    AI: Ai;
+}
 
 export default {
-    async fetch(request, env, ctx) {
+    async fetch(request: Request, env: Env, ctx: ExecutionContext) {
         const motd = await env.KV.get("MOTD") ?? await this.updateMOTD(env);
 
-        return new Response(this.buildSVG(motd), {
+        return new Response(this.buildSVG(motd as string), {
             headers: {
                 "Content-Type": "image/svg+xml",
                 "Cache-Control": "no-cache, no-store, must-revalidate"
@@ -12,11 +18,11 @@ export default {
         });
     },
 
-    async scheduled(controller, env, ctx) {
+    async scheduled(controller: ScheduledController, env: Env, ctx: ExecutionContext) {
         await this.updateMOTD(env);
     },
 
-    async updateMOTD(env) {
+    async updateMOTD(env: Env) {
         const res = await env.AI.run("@cf/meta/llama-3.2-1b-instruct", {
             messages: [
                 {
@@ -32,12 +38,12 @@ export default {
             temperature: 0.7
         });
 
-        await env.KV.put("MOTD", res.response);
+        await env.KV.put("MOTD", res.response as string);
 
         return res.response;
     },
 
-    buildSVG(text) {
+    buildSVG(text: string) {
         return `
             <svg viewBox="0 0 500 100" xmlns="http://www.w3.org/2000/svg">
                 <foreignObject x="0" y="0" width="500" height="100">
